6266444a0d1ca2ce327ef5348c826ccf2fbee42d
John Cabia-an <cabia-anjo_sd2053@ceaccs.net>
2019-12-06 18:17:27 +0800

    updated

---
+++ loginform/src/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+<div id="app">
+    <v-app id="inspire">
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4>
+                        <v-card class="elevation-12">
+                            <v-toolbar color="blue" dark flat>
+                                <v-toolbar-title>Login form</v-toolbar-title>
+                                <v-spacer></v-spacer>
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field label="Login" v-model="username" name="login" prepend-icon="mdi-account" type="text"></v-text-field>
+
+                                    <v-text-field id="password" label="Password" v-model="password" name="password" prepend-icon="mdi-lock" type="password"></v-text-field>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="blue">Login</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
+</div>
+</template>
+
+<script>
+export default {
+        data(){
+            return{
+                username:"",
+                password:""
+            }
+        }
+}
+</script>
+
+<style>
+
+</style>
